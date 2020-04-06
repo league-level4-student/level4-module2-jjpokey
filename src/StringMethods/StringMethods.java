3fcf7edbfea5579c71294e79142d5b676984049c
Jason <jasonelliott99@yahoo.com>
2020-04-06 16:58:42 -0700

    string methods

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -1,5 +1,7 @@
 package StringMethods;
 
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
 import java.util.Arrays;
 import java.util.Base64;
 
@@ -32,13 +34,26 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+int i1 = s1.length();
+int i2 = s2.length();
+if(i1 > i2) {
+	return s1;
+}
+else if(i1 < i2){
+	return s2;
+}
+return s1;
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
+		String s2 = s;
+		if(s.contains("underscores")) {
+			s2 = s.replace(' ', '_');
+		}
+		System.out.println(s2);
+		return s2;
 	}
 
 	
@@ -46,13 +61,64 @@ public class StringMethods {
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		s1 = s1.trim();
+		s2 = s2.trim();
+		s3 = s3.trim();
+		
+		String[] s11 = s1.split(" ");
+		String[] s12 = s2.split(" ");
+		String[] s13 = s3.split(" ");
+		
+		if(s11[1].compareTo(s12[1]) < 0 && s1.compareTo(s13[1]) < 0) {
+			return s1;
+		}
+		else if(s12[1].compareTo(s11[1]) < 0 && s12[1].compareTo(s13[1]) < 0) {
+			return s2;
+		}
+		else if(s13[1].compareTo(s11[1]) < 0 && s13[1].compareTo(s12[1]) < 0) {
+			return s3;
+		}
+		return s1;
 	}
 	
 	
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int total = 0;
+		for(int i = 0; i < s.length(); i++) {
+			
+		if(s.charAt(i) == '1') {
+			total +=1;
+		} 
+		else if(s.charAt(i) == '2') {
+			total +=2;
+		}
+		else if(s.charAt(i) == '3') {
+			total +=3;
+		}
+		else if(s.charAt(i) == '4') {
+			total +=4;
+		} 
+		else if(s.charAt(i) == '5') {
+			total +=5;
+		}
+		else if(s.charAt(i) == '6') {
+			total +=6;
+		}
+		else if(s.charAt(i) == '7') {
+			total +=7;
+		}
+		else if(s.charAt(i) == '8') {
+			total +=8;
+		}
+		else if(s.charAt(i) == '9') {
+			total +=9;
+		}
+		else {
+			total += 0;
+		}
+		}
+		return total;
 	}
 	
 	
